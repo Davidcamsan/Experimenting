0006a4403bec38047a798d94540e4d9787d64094
David Cambronero <Davidcamsan16@gmail.com>
2018-04-14 08:22:19 -0600

    Update
    Update

---
+++ Experimenting/ViewModels/GradeToPercentConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Experimenting.ViewModels
+{
+    public class GradeToPercentConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return String.Format("{0}%", value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
--- Experimenting/obj/Debug/Experimenting.Views.EstudianteEspecificoView.xaml.g.cs
+++ Experimenting/obj/Debug/Experimenting.Views.EstudianteEspecificoView.xaml.g.cs
@@ -16,8 +16,12 @@ namespace Experimenting.Views {
     public partial class EstudianteEspecificoView : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.ListView lstTareas;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(EstudianteEspecificoView));
+            lstTareas = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ListView>(this, "lstTareas");
         }
     }
 }
